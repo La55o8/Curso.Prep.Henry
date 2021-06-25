9e90c7eb3cc9ff1e2e823c3ac6e970100e7b14da
Manuel Ignacio Lasso Podesta <manulasso2000@hotmail.com>
2021-06-25 11:18:25 -0300

    All tasks corrected

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str;
 }
 
 function suma(x, y) {
@@ -59,8 +60,11 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-   if (x == y){
-    return true
+   if (x === y){
+    return true;
+  }
+  else {
+    return false;
   }
   
 }
@@ -69,8 +73,11 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 == str2){
-    returntrue
+  if (str1.length === str2.length){
+    return true
+  }
+  else {
+    return false;
   }
 }
 
@@ -81,6 +88,9 @@ function menosQueNoventa(num) {
   if (num<90){
     return true
   }
+  else {
+    return false;
+  }
 }
 
 function mayorQueCincuenta(num) {
@@ -89,6 +99,9 @@ function mayorQueCincuenta(num) {
   // Tu código:
   if (num>50){
     return true
+  }
+  else {
+    return false;
   }
 }
 
@@ -105,6 +118,9 @@ function esPar(num) {
   if (num%2 == 0){
     return true
   }
+  else {
+    return false;
+  }
 }
 
 function esImpar(num) {
@@ -113,6 +129,10 @@ function esImpar(num) {
   // Tu código:
   if (num % 2 == 1){
     return true
+  }
+  else {
+    return false;
+  }
 }
 
 function elevarAlCuadrado(num) {
@@ -143,7 +163,7 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return Math.floor(num);
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
@@ -157,11 +177,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero<0){
-    return "Es postivo";
+  if (numero === 0){
+    return false;
+  }
+  if (numero>0){
+    return "Es positivo";
   }
   else{
-    return "Es neagtivo";
+    return "Es negativo";
   }
 }
 
@@ -169,21 +192,22 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return console.log(str+'!');
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return console.log(nombre + ' ' + apellido);
+  var combo = nombre + ' ' + apellido;
+  return combo;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return console.log('Hola ' + nombre + '!')
+  return 'Hola ' + nombre + '!'
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -196,7 +220,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return lado**4;
+  return lado*4;
 }
 
 
@@ -221,12 +245,12 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra = 1){
-    console.log("Dato Incorrecto")
+  if (letra.length != 1){
+    return "Dato incorrecto"
   }
   else{
-    if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ){
-      console.log("Es vocal");
+    if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ){
+      return "Es vocal";
     }
   }
 }
